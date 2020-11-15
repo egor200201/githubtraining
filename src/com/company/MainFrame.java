46bba74909047a145f9c52aa840613f2a852df44
egor200201 <egor20020130@outlook.com>
2020-11-16 02:20:16 +0300

    commit

---
+++ src/com/company/MainFrame.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class MainFrame {
+    public static void main(String[] args) {
+        // Display "Hello World!"
+        System.out.println("Hello World!");
+    }
+}
+
